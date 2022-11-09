7d6cfb079beb6b28b54630bfde694f7e5391263e
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2022-11-09 14:53:04 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+float avg;
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+avg=(a+b+c)/3;
+printf("%f",avg);
+}
---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a no:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d is an even no",a);
+}
+else
+{
+printf("%d is odd",a);
+}
+}
