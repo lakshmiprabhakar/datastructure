7f0d6a7be20d8717b30f1b2671f58f5c3acc5892
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2022-12-01 11:47:13 +0530

    Add files via upload

---
+++ mergesort.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+void main()
+{
+int a[50],b[50],c[100],m,n,i,j,k;
+printf("Enter the size of first array:\n");
+scanf("%d",&m);
+printf("Enter first array elements:\n");
+for(i=0;i<m;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Enter the size of second array:\n");
+scanf("%d",&n);
+printf("Enter second array elements:\n");
+for(j=0;j<n;j++)
+{
+scanf("%d",&b[j]);
+}
+i=0;
+j=0;
+k=0;
+while(i<m&&j<n){
+if(a[i]<b[j])
+{
+  c[k]=a[i];
+  i++;
+}
+else
+{
+  c[k]=b[j];
+  j++;
+}
+k++;
+}
+if(i>=m){
+while(j<n){
+c[k]=b[j];
+j++;
+k++;
+}
+}
+if(j>=n){
+while(i<m){
+c[k]=a[i];
+i++;
+k++;
+}
+}
+printf("After Merging\n");
+for(i=0;i<k;i++)
+{
+printf("%d",c[i]);
+}
+}
--- stack.c
+++ stack.c
@@ -1,48 +1,65 @@
 #include<stdio.h>
-int stack[100],choice,n,top,x,i;
-void push(void);
+
+int stack[100],choice,n,top,x,i; //global declaration
+
+void push(void);                // function declaration
 void pop(void);
 void display(void);
+
+
 void main()
 {
+
 top=-1;
+
+
+
 printf("\nEnter the size of stack[MAX=100]:");
 scanf("%d",&n);
+
 printf("\n\t STACK OPERATIONS USING ARRAY:");
 printf("\n\t.........");
 printf("\n\t 1.PUSH \n\t 2.POP \n\t 3.DISPLAY\n\t 4.EXIT\n\t");
+
+
+
 do
 {
 printf("\nEnter the Choice:");
 scanf("%d",&choice);
+
+
 switch(choice)
 {
 case 1:
-{
+
 push();
 break;
-}
+
 case 2:
-{
+
 pop();
 break;
-}
+
 case 3:
-{
+
 display();
 break;
-}
+
 case 4:
-{
+
 printf("\n\tEXIT POINT");
 break;
-}
+
 default:
 {
 printf("\n\tPlease enetr a valid choice(1/2/3/4)");
 }
+
 }
-while(choice!=4)
+}
+while(choice!=4);
+
 }
 void push()
 {
@@ -60,13 +77,17 @@ stack[top]=x;
 }
 void pop()
 {
-if(top<=-1)
+if(top==-1){
+printf("\n\tstack is underflow");
+}
+if(top>-1)
 {
 printf("\n\tThe popped elements is %d",stack[top]);
 top--;
+display();
 }
 }
-void diplay()
+void display()
 {
 if(top>=0)
 {
